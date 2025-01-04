a1d1b18313c11bf36820c738be35fc90d1835b9b
Zephyr <46925806+Zephyr0u@users.noreply.github.com>
2025-01-04 16:11:18 -0300

    Update max_money.py

--- code/max_money.py
+++ code/max_money.py
@@ -8,6 +8,8 @@ def max_money():
     # 50 BTC = 50 0000 0000 Satoshis
     current_reward = 50 * 10**8
     total = 0
+        current_reward = 50 * 10**8
+
     while current_reward > 0:
         total += reward_interval * current_reward
         current_reward /= 2
